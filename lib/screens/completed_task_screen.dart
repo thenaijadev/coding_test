54c3958802eb8a757ca4684b8c54208303336881
Chukwuemeka  Okwor <chukwuemekaokwor@Chukwuemekas-MacBook-Air.local>
2024-03-13 22:53:37 +0100

    Done

--- lib/screens/completed_task_screen.dart
+++ lib/screens/completed_task_screen.dart
@@ -1,4 +1,5 @@
 import 'package:coding_test/core/widgets/text_widget.dart';
+import 'package:coding_test/widgets/completed_task_widget.dart';
 import 'package:flutter/material.dart';
 
 class CompletedTaskScreen extends StatelessWidget {
@@ -35,64 +36,5 @@ class CompletedTaskScreen extends StatelessWidget {
             ],
           ),
         ));
-  }
-}
-
-class CompletedTasks extends StatelessWidget {
-  const CompletedTasks(
-      {super.key, required this.title, required this.subTitle});
-  final String title;
-  final String subTitle;
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 22.0),
-      child: Container(
-        width: double.infinity,
-        padding: const EdgeInsets.symmetric(vertical: 22, horizontal: 18),
-        decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(15),
-            boxShadow: [
-              const BoxShadow(
-                color: Color.fromARGB(114, 117, 117, 117),
-                spreadRadius: 1,
-                blurRadius: 5,
-                offset: Offset(0, 5),
-              ),
-              BoxShadow(
-                color: Colors.grey.shade300,
-                offset: const Offset(-5, 0),
-              ),
-              BoxShadow(
-                color: Colors.grey.shade300,
-                offset: const Offset(5, 0),
-              )
-            ]),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                TextWidget(
-                  text: title,
-                  fontSize: 13,
-                  color: Theme.of(context).colorScheme.primary,
-                  fontWeight: FontWeight.bold,
-                ),
-                const SizedBox(
-                  height: 8,
-                ),
-                TextWidget(
-                  text: subTitle,
-                  fontSize: 10,
-                )
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
   }
 }
---
+++ lib/widgets/completed_task_widget.dart
@@ -0,0 +1,61 @@
+import 'package:coding_test/core/widgets/text_widget.dart';
+import 'package:flutter/material.dart';
+
+class CompletedTasks extends StatelessWidget {
+  const CompletedTasks(
+      {super.key, required this.title, required this.subTitle});
+  final String title;
+  final String subTitle;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 22.0),
+      child: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.symmetric(vertical: 22, horizontal: 18),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(15),
+            boxShadow: [
+              const BoxShadow(
+                color: Color.fromARGB(114, 117, 117, 117),
+                spreadRadius: 1,
+                blurRadius: 5,
+                offset: Offset(0, 5),
+              ),
+              BoxShadow(
+                color: Colors.grey.shade300,
+                offset: const Offset(-5, 0),
+              ),
+              BoxShadow(
+                color: Colors.grey.shade300,
+                offset: const Offset(5, 0),
+              )
+            ]),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextWidget(
+                  text: title,
+                  fontSize: 13,
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold,
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                TextWidget(
+                  text: subTitle,
+                  fontSize: 10,
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
